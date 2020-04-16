c4044755e09851bf44c3e3f71ce95777864598ed
Aiman Nadzmi <aiman_nadzmi@hotmail.com>
2020-04-16 15:43:27 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return <Button color="primary">Hello World</Button>;
+}
+export default App;
